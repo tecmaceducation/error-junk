23f1f37cac3c62f56d8a8666ab02506ce5d655e6
Dhanush <tecmaceducation@gmail.com>
2025-06-22 02:29:33 +0530

    favicon

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/", // 👈 Add this line for GitHub Pages
+
   server: {
     host: "::",
     port: 8080,
